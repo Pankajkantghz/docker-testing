84fe4b3aebdb423aafa249078e17c8ac28707735
Pankaj Yadav <pankaj.py96810@gmail.com>
2026-04-03 09:29:31 +0530

    added to the github

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+import { prisma } from "./lib/prisma.js";
+
+const app = express();
+
+app.use(express.json());
+
+// GET all users
+
+app.get("/", async (req, res) => {
+  const data = await prisma.user.findMany();
+
+  res.json({ data });
+});
+
+// CREATE user
+
+app.post("/", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: Math.random().toString(),
+
+      password: Math.random().toString(),
+    },
+  });
+
+  res.json({ user });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
